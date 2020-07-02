90bf42b3977d7438fc75b939f133ba9d0c45f72a
lantler <54602963+lantler@users.noreply.github.com>
2020-07-01 22:40:32 -0400

    firstpush

---
+++ Overview.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills</title>
+    <link rel="stylesheet" href="../../css/foundation.css">
+    <link rel="stylesheet" href="../../css/allpages.css">
+    <link rel="stylesheet" href="https://use.typekit.net/agr6hpy.css">
+  </head>
+  <body>
+
+<!-- NOTE: TOP BAR -->
+<div class="multilevel-offcanvas off-canvas position-right" id="offCanvasRight" data-off-canvas>
+  <!-- NOTE: vertical menu -->
+  <ul class="vertical menu" data-accordion-menu>
+    <li><a href="../../players.html">Players</a>
+      <ul class="menu vertical nested" style="font-weight: 300">
+        <li><a href="../../charactercreation/index.php">Character Creation</a></li>
+        <li><a href="../../feats/index.php">Feats</a></li>
+        <li><a href="../../gear/index.php">Gear</a></li>
+        <li><a href="../../objects/index.php">Objects</a></li>
+        <li><a href="../../skillfeats/index.php">Skill Feats</a></li>
+        <li><a href="../../spelleffects/index.php">Spell Effects</a></li>
+       </ul>
+    </li>
+    <li><a href="../gamemasters/gamemasters.html">Gamemasters</a>
+      <ul class="menu vertical nested"style="font-weight: 300">
+        <li><a href="../../gamemasters/balancingencounters/index.php">Balancing Encounters</a></li>
+        <li><a href="../../gamemasters/beastcreation/index.php">Beast Creation</a></li>
+        <li><a href="../../gamemasters/beastiary/index.php">Beastiary</a></li>
+        <li><a href="../../gamemasters/factions/index.php">Factions</a></li>
+        <li><a href="../../gamemasters/runningcampaign/index.php">Running a Campaign</a></li>
+       </ul>
+     </li>
+    <li><a href="../rules/rules.html">Rules</a>
+      <ul class="menu vertical nested" style="font-weight: 300">
+        <li><a href="../general/index.php">General</a></li>
+        <li><a href="../combat/index.php">Combat</a></li>
+        <li><a href="../incapacitation/index.php">Incapacitation</a></li>
+        <li><a href="../skills/index.php">Skills</a></li>
+        <li><a href="../spells/index.php">Spells</a></li>
+        <li><a href="../vehicles/index.php">Vehicles</a></li>
+       </ul>
+    </li>
+   </ul>
+  <ul class="vertical menu">
+    <li><a href="#">Contact Us</a></li>
+  </ul>
+
+  <ul class="menu simple social-links">
+    <li><a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+    <li><a href="#" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+    <li><a href="#" target="_blank"><i class="fa fa-github-square" aria-hidden="true"></i></a></li>
+    <li><a href="#" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+  </ul>
+</div>
+
+<!-- NOTE: bar content -->
+
+<div class="off-canvas-content" data-off-canvas-content>
+  <div class="nav-bar">
+      <div class="nav-bar-left">
+        <a href="../../index.html"class="nav-bar-logo"><img class="logo" src=../../fragmentsseal.png width="40px" height="40px"></a>
+      </div>
+
+  <!-- NOTE: nav bar right -->
+
+      <div class="nav-bar-right">
+        <ul class="menu">
+          <li class="hide-for-small-only"><a href="../../players/players.html">Players</a></li>
+          <li class="hide-for-small-only"><a href="../../gamemasters/gamemasters.html">Gamemasters</a></li>
+<!-- NOTE: search -->
+
+        <ul class="menu search">
+          <li><input type="search" placeholder=""></li>
+          <li><button type="button" class="button" style="background-color: #3c5178;">Search</button></li>
+        </ul>
+<!-- NOTE: end search -->
+          <li>
+            <button class="offcanvas-trigger" type="button" data-open="offCanvasRight">
+              <div class="hamburger">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+              </div>
+            </button>
+          </li>
+        </ul>
+    </div>
+
+  </div>
+ </div>
+
+ <!-- NOTE: bar content -->
+
+<!-- NOTE: HEADER -->
+  <div class="holy-grail-grid">
+
+      <div class="holy-grail-left">
+        <h3>Rules</h3>
+        <ul>
+          <li><a href="../general/index.html">General</a></li>
+          <li><a href="../general/index.html">General</a></li>
+          <li><a href="../incapacitation/index.html">Incapacitation</a></li>
+          <li><a href="../skills/index.html">Skills</a></li>
+          <li><a href="../spells/index.html">Spells</a></li>
+          <li><a href="../vehicles/index.html">Vehicles</a></li>
+        </ul>
+      </div>
+
+      <div class="holy-grail-middle">
+        <h3>Skills</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias mollitia debitis at atque temporibus ratione molestiae id eos, cupiditate numquam dolor vero totam, quisquam libero voluptas. Animi ipsa cumque, tempore.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias mollitia debitis at atque temporibus ratione molestiae id eos, cupiditate numquam dolor vero totam, quisquam libero voluptas. Animi ipsa cumque, tempore.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias mollitia debitis at atque temporibus ratione molestiae id eos, cupiditate numquam dolor vero totam, quisquam libero voluptas. Animi ipsa cumque, tempore.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias mollitia debitis at atque temporibus ratione molestiae id eos, cupiditate numquam dolor vero totam, quisquam libero voluptas. Animi ipsa cumque, tempore.</p>
+      </div>
+  </div>
+
+<!-- NOTE: FOOTER -->
+<footer></footer>
+    <script src="../../js/vendor/jquery.js"></script>
+    <script src="../../js/vendor/what-input.js"></script>
+    <script src="../../js/vendor/foundation.js"></script>
+    <script src="../../js/app.js"></script>
+  </body>
+</html>
